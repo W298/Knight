8260c9c63967df8ff8b0054d7f2cb7e63d187fa3
RUKA <lutca1320@gmail.com>
2020-11-15 21:18:33 +0900

    Remake using Blueprint, not yet C++

---
+++ Source/Knight/KnightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KnightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KNIGHT_API AKnightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
